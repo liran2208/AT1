6836327571c9b2b0f1e94380c892588ef891073d
liran2208 <65400001+liran2208@users.noreply.github.com>
2021-02-16 21:18:24 +0200

    Create SM_brute.py
    using brute force to know the distance between two nodes in a graph presented in a adjacency list

---
+++ SM_brute.py
@@ -0,0 +1,31 @@
+from collections import deque
+
+# small world problem
+
+def d_two_nodes_graph(g, num1, num2):
+    count = 0
+    q = deque()
+    q.append(g[num1-1])
+    current = 0
+    while count < len(g):
+        count += 1
+        current = q.popleft()
+
+        if (num2 in current):
+            return count
+        else:
+            for i in current:
+                q.append(g[i-1])
+    return 404
+
+#g = [[2, 4], [1, 3], [4, 2], [3, 1]]
+g = [ [2, 5], 
+      [6, 5, 4], 
+      [6, 9, 4], 
+      [2, 3, 5, 8, 9], 
+      [2, 3, 8, 7], 
+      [6, 9], 
+      [6, 5, 4], 
+      [5, 3, 4, 7]]
+
+print (d_two_nodes_graph(g, 1, 5))
