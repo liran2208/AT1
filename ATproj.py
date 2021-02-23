0d192d4f6f0aa0741ac48f38ca7d67c69d5f5312
liran2208 <65400001+liran2208@users.noreply.github.com>
2021-02-23 23:18:38 +0200

    Update ATproj.py

--- ATproj.py
+++ ATproj.py
@@ -1,11 +1,22 @@
-V0 = []
-V1 = []
-V2 = []
+V0 = {0:(1,2)}
+V1 = {0:(1, 4, 5), 1:(2, 6), 2:(3), 3:(0), 4:(1), 5:(2)}
+V2 = {0:(1, 4, 5), 1:(2, 6), 2:(3, 7), 3:(7), 4:(1), 5:(2), 7:(3), 8:(1, 2), 9:(0, 4, 5, 6, 7, 3)}
 
-E0 = range(0, 2)
-E1 = range(0, 6)
-E2 = range(0, 9)
+E0 = list(range(0, 2))
+E1 = list(range(0, 6))
+E2 = list(range(0, 9))
+
 EX_GRAPH0 = (E0, V0)
 EX_GRAPH1 = (E1, V1)
 EX_GRAPH2 = (E2, V2)
 
+def make_complete_graph(num_nodes):
+    # which is:
+
+    # for i in list(range(num_nodes)):
+    #     v[i] = list(range(num_nodes))
+    #     v[i].pop(i)
+    
+    return (list(range(num_nodes)), {i: (list(set(list(range(num_nodes)))-set([i]))) for i in list(range(num_nodes))})
+
+print (make_complete_graph(5))
